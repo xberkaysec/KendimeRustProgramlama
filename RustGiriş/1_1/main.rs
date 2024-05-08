892bcfd8e7bbc37c0d1d642a4a111d56bdab9177
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-09 01:19:41 +0300

    Update main.rs

--- RustGiriş/1_1/main.rs
+++ RustGiriş/1_1/main.rs
@@ -1,1 +1,14 @@
+fn factorial(n:i32) -> i32 {
+	match n {
+		0..=1=>1,
+		_=> n*factorial(n-1)
+	}
+}
 
+fn main() {
+	let num = 4;
+	let result = factorial(num);
+	println!("{} sayısının faktöriyeli: {}", num, result);
+}
+
+// Çıktısı - 4 sayısının faktöriyeli: 24 
