135e1755c08c2e9047703cf56bd94c24bbe7d9df
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-09 01:34:04 +0300

    Create main.rs

---
+++ RustGiriş/1_2/main.rs
@@ -0,0 +1,11 @@
+fn check_even_odd(num: i32) {
+    match num {
+        n if n % 2 == 0 => println!("{} çifttir.", n),
+        _ => println!("{} tektir.", num),
+    }
+}
+
+fn main() {
+    check_even_odd(5);
+    check_even_odd(10);
+}
