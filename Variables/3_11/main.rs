28762e02b35c5e2ecce95cec1f4a31d661e57285
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-19 14:24:29 +0300

    Create main.rs

---
+++ Variables/3_11/main.rs
@@ -0,0 +1,22 @@
+// Pointers 
+
+use std::env::consts;
+
+fn main() {
+    
+    let a: i32 = 10;
+    // 10
+    let b: i32 = 20;
+    // 20
+    let c: i32 = 30;
+
+    let pa = &a as *const i32;
+    // 0x36d52ff4d0
+    let pb = &b as *const i32;
+    // 0x36d52ff4d4 
+    let pc: *const i32 = &c as *const i32;
+    // 0x7fff149df27c
+    
+    println!("{:?} {:?} {:?}", pa, pb, pc);
+
+}
