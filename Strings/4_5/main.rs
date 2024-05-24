54b700265a2a90e048427526e2bc3109df20d177
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-24 13:18:07 +0300

    Create main.rs

---
+++ Strings/4_5/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    
+    let english = "Hello".to_string();
+    let greek = "γεια".to_string();
+    let chinese = "你好".to_string();
+
+    println!(
+        "İngilizce {}: Bayt {} Uzunluk {}",
+        english,
+        english.len(),
+        english.chars().count()
+    );
+
+    println!(
+        "Yunanca {}: Bayt {} Uzunluk {}",
+        greek,
+        greek.len(),
+        greek.chars().count()
+    );
+
+    println!(
+        "Çince {}: Bayt {} Uzunluk {}",
+        chinese,
+        chinese.len(),
+        chinese.chars().count()
+    );
+
+}
