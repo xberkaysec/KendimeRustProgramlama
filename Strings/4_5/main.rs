0e0c913538d1777535c30013924da5b5e9b0b7d2
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-24 13:20:05 +0300

    Update main.rs

--- Strings/4_5/main.rs
+++ Strings/4_5/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let english = "Hello".to_string();
     let greek = "γεια".to_string();
     let chinese = "你好".to_string();
+    let turkish = "Merhaba".to_string();
 
     println!(
         "İngilizce {}: Bayt {} Uzunluk {}",
@@ -23,6 +24,13 @@ fn main() {
         chinese,
         chinese.len(),
         chinese.chars().count()
+    );
+
+    println!(
+        "Türkçe {}: Bayt {} Uzunluk {}",
+        turkish,
+        turkish.len(),
+        turkish.chars().count()
     );
 
 }
