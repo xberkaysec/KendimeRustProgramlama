398c490c1cdebdc6ef2da901b27db2bca41ad061
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-25 19:48:29 +0300

    Create main.rs

---
+++ Strings/4_6/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    let mut alfabe = "a ".to_string();
+    alfabe.push('b');
+
+    let mut sayılar = "bir ".to_string();
+    sayılar.push_str("iki");
+
+    println!("{} | {}", alfabe, sayılar);
+
+}
