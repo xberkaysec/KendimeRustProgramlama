c54148f16cc54b1be733619e5e2d24eeae515615
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-26 18:26:40 +0300

    Create main.rs

---
+++ Strings/4_19/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let string_1 = "Can eve gitti, ardından hemen Can evden çıktı.".to_string();
+    let sonuc = string_1.replace("Can", "Berkay");
+
+    println!("Yeni cümle : {}", sonuc);
+  
+}
