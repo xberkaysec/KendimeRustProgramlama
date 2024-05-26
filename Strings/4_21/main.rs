a3fb776af408b6bc4fb28837d21eec6ed8d01fa2
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-26 18:40:16 +0300

    Create main.rs

---
+++ Strings/4_21/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let string_1 = "Ankara çok soğuk.";
+    let sonuc = string_1.starts_with("Ankara");
+
+    println!("\"Ankara\", \"{string_1}\" cümlesinin başlangıcında = {sonuc}");
+
+}
