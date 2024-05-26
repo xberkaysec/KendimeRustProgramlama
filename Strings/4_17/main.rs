b7a5b071d93b5c1fa5acad9b6327e383d768636b
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-05-26 18:14:36 +0300

    Create main.rs

---
+++ Strings/4_17/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let string_1 = "Kim Bu Berkay".to_string();
+    let sonuc = string_1.ends_with("Berkay");
+    
+    println!("\"{string_1}\" cümlesi için \"Berkay\" sonucu: {}", sonuc);
+
+}
