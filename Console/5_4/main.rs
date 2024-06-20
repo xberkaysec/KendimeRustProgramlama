8624b6e5eac2e3c8fe978c390c5bdb37d4c2225a
Berkay AĞGÜL <rbrky8787@gmail.com>
2024-06-20 20:47:56 +0300

    Create main.rs

---
+++ Console/5_4/main.rs
@@ -0,0 +1,9 @@
+#[warn(unused_mut)]
+
+fn main() {
+
+    let (ilk, iki, üc, dört) = (1, 2, 3, 4);
+    let sonuc = ilk + iki + üc + dört;
+    println!("{ilk} + {iki} + {üc} + {dört} = {sonuc}");
+
+}
